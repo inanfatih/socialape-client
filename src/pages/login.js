b036b57465187726c5abb28b319f69e44801058e
Fatih <mfatihinan@gmail.com>
2020-03-21 22:56:23 -0400

    initial react app cleaned up and pages are created

---
+++ src/pages/login.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class login extends Component {
+  render() {
+    return <div>login Page</div>;
+  }
+}
+
+export default login;
---
+++ src/pages/signup.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class signup extends Component {
+  render() {
+    return <div>signup Page</div>;
+  }
+}
+
+export default signup;
