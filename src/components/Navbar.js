d05f840bd7957fe0ac172c36637687c47a797e66
Fatih <mfatihinan@gmail.com>
2020-03-23 00:17:28 -0400

    started working on home page. Cards are created and data displayed on them

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,32 @@
 import React, { Component } from 'react';
 
+import Link from 'react-router-dom/Link';
+
+//MUI => Material UI staff
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Button from '@material-ui/core/Button';
+
+//Yukaridaki yerine asagidaki sekilde tum material-ui/core u import edersek, compile zamaninda tum library'i indirmek zorunda kaliriz. Bu da compile time'da hizi dusuruyor.
+//import {AppBar, ToolBar} from '@material-ui/core';
+
 class Navbar extends Component {
   render() {
-    return <div></div>;
+    return (
+      <AppBar position='fixed'>
+        <Toolbar className='nav-container'>
+          <Button color='inherit' component={Link} to='/login'>
+            Login
+          </Button>
+          <Button color='inherit' component={Link} to='/'>
+            Home
+          </Button>
+          <Button color='inherit' component={Link} to='/signup'>
+            Sigup
+          </Button>
+        </Toolbar>
+      </AppBar>
+    );
   }
 }
 
--- src/pages/home.js
+++ src/pages/home.js
@@ -1,8 +1,42 @@
 import React, { Component } from 'react';
+import Grid from '@material-ui/core/Grid';
+import axios from 'axios';
+
+import Scream from '../components/Scream';
 
 class home extends Component {
+  state = {
+    screams: null,
+  };
+
+  componentDidMount() {
+    axios
+      .get('./screams')
+      .then(res => {
+        console.log(res.data);
+        console.log(res);
+        this.setState({ screams: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
   render() {
-    return <div>Home Page</div>;
+    let recentScreamsMarkup = this.state.screams ? (
+      this.state.screams.map(scream => <Scream scream={scream} />)
+    ) : (
+      <p>Loading...</p>
+    );
+    return (
+      <Grid container spacing={4}>
+        <Grid item sm={8} xs={12}>
+          {recentScreamsMarkup}
+        </Grid>
+        <Grid item sm={4} xs={12}>
+          <p>Profile</p>
+        </Grid>
+      </Grid>
+    );
   }
 }
 
