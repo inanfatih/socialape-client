a9f524df505c9a456cc2ed1b5f0c0b12a2a2d7ec
Fatih <mfatihinan@gmail.com>
2020-03-24 23:56:07 -0400

    started working on Login page

--- src/components/Scream.js
+++ src/components/Scream.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
-import Link from 'react-router-dom/Link';
+import { Link } from 'react-router-dom';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 //MUI stuff
 import Card from '@material-ui/core/Card';
@@ -24,6 +26,7 @@ const styles = {
 
 class Scream extends Component {
   render() {
+    dayjs.extend(relativeTime);
     const {
       classes,
       scream: {
@@ -44,7 +47,7 @@ class Scream extends Component {
           title='Profile Image'
           className={classes.image}
         />
-        <CardContent class={classes.content}>
+        <CardContent className={classes.content}>
           <Typography
             variant='h5'
             component={Link}
@@ -53,7 +56,7 @@ class Scream extends Component {
             {userHandle}
           </Typography>
           <Typography variant='body2' color='textSecondary'>
-            {createdAt}
+            {dayjs(createdAt).fromNow()}
           </Typography>
           <Typography variant='body1'>{body}</Typography>
         </CardContent>
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,9 +1,41 @@
 import React, { Component } from 'react';
+import withStyles from '@material-ui/core/styles/withStyles';
+import PropTypes from 'prop-types'; //built in react for type checking. Minimizes potential errors
+import AppIcon from '../images/Monkey.png';
 
+//MUI stuff
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
+const styles = {
+  form: {
+    textAlign: 'center',
+  },
+  image: {
+    margin: '20px auto 20px auto',
+    width: 200,
+  },
+};
 class login extends Component {
   render() {
-    return <div>login Page</div>;
+    const { classes } = this.props;
+    return (
+      <Grid container className={classes.form}>
+        <Grid item sm></Grid>
+        <Grid item sm>
+          <img src={AppIcon} alt='Monkey' className={classes.image} />
+          <Typography variant='h2' className={classes.pageTitle}>
+            Login
+          </Typography>
+        </Grid>
+        <Grid item sm></Grid>
+      </Grid>
+    );
   }
 }
 
-export default login;
+login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(login);
